7d5e0e733d4ccad10516f138ca44ff2448a0dc67
changye01 <changye01@users.noreply.github.com>
2022-11-08 09:21:52 +0000

    deploy: cf1e18826452d1ebfc4c994b2952442a73a02e9f

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,16 +1,4 @@
-import Darkmode from 'darkmode-js';
-
-const options = {
-    bottom: '64px', // default: '32px'
-    right: 'unset', // default: '32px'
-    left: '32px', // default: 'unset'
-    time: '0.5s', // default: '0.3s'
-    mixColor: '#fff', // default: '#fff'
-    backgroundColor: '#fff',  // default: '#fff'
-    buttonColorDark: '#100f2c',  // default: '#100f2c'
-    buttonColorLight: '#fff', // default: '#fff'
-    saveInCookies: false, // default: true,
-    label: '🌓', // default: ''
-    autoMatchOsTheme: true // default: true
+function addDarkmodeWidget() {
+    new Darkmode().showWidget();
   }
-new Darkmode(options).showWidget();
+window.addEventListener('load', addDarkmodeWidget);
